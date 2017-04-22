95c2b5ea4c8639f3f3042f57c32def2a1b3d7054
Andrew <email@andrewlord.co.uk>
2017-04-22 18:43:43 +0100

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,13 @@
+var React = require('react'),
+    Nav = require('Nav');
+
+var Main = (props) => {
+  return (
+    <div>
+      <Nav/>
+      <p>Main.jsx rendered</p>
+    </div>
+  );
+};
+
+module.exports = Main;
---
+++ app/components/Nav.jsx
@@ -0,0 +1,26 @@
+var React = require('react'),
+    {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function()
+  {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="dropdown menu" data-dropdown-menu="">
+            <li className="menu-text">React Timer App</li>
+            <li><IndexLink to="/" activeClassName="active">Timer</IndexLink></li>
+            <li><Link to="/countdown" activeClassName="active">Countdown</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Created by <a href="https://github.com/amlord/" title="Andrew Lord GitHub Profile" target="_blank">Andrew Lord</a></li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Nav;
